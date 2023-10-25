7fad5739de1b15ac7315a4591d6bf1ecb6c7510d
Laurell Seville <laurellseville2000@gmail.com>
2023-10-25 15:29:22 +0100

    feat, add binary search

---
+++ searching/binary-search.js
@@ -0,0 +1,38 @@
+/*
+Binary Search Exercise
+Write a function called binarySearch which accepts a sorted array and a value and returns the index at which the value exists. Otherwise, return -1.
+
+This algorithm should be more efficient than linearSearch - you can read how to implement it here - https://www.khanacademy.org/computing/computer-science/algorithms/binary-search/a/binary-search and here - https://www.topcoder.com/community/data-science/data-science-tutorials/binary-search/
+*/
+
+function binarySearch(arr, value) {
+  // add whatever parameters you deem necessary - good luck!
+
+  let start = 0;
+  let end = arr.length - 1;
+
+  console.log("start: " + start);
+  console.log("end: " + end);
+
+  while (start < end) {
+    let mid = Math.ceil((start + end) / 2);
+
+    let midVal = arr[mid];
+
+    console.log("midVal: " + midVal);
+
+    if (midVal === value) {
+      return mid;
+    } else if (arr[mid] < value) {
+      start++;
+    } else {
+      end--;
+    }
+  }
+
+  return -1;
+}
+
+const result = binarySearch([1, 2, 3, 4, 5], 5);
+
+console.log(result);
