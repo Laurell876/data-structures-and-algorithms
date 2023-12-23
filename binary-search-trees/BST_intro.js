acb492d8a482d14cf6c4d7257cfa772fa2680640
Laurell Seville <laurellseville2000@gmail.com>
2023-12-23 16:51:45 +0000

    feat, add dfs

--- binary-search-trees/BST_intro.js
+++ binary-search-trees/BST_intro.js
@@ -1,95 +1,114 @@
 class Node {
-    constructor(value) {
-        this.value = value;
-        this.left = null; // has smaller value
-        this.right = null; // has larger value
-    }
+  constructor(value) {
+    this.value = value;
+    this.left = null; // has smaller value
+    this.right = null; // has larger value
+  }
 }
 
 class BinarySearchTree {
-    constructor() {
-        this.root = null
-    }
+  constructor() {
+    this.root = null;
+  }
 
-    insert(value) {
-        var newNode = new Node(value)
-        if (this.root === null) {
-            this.root = newNode;    
+  insert(value) {
+    var newNode = new Node(value);
+    if (this.root === null) {
+      this.root = newNode;
+      return this;
+    } else {
+      let current = this.root;
+      while (true) {
+        if (newNode.value === current.value) {
+          // This tree ignores duplicates, a counter can be added to nodes in the future
+          return this;
+        } else if (newNode.value < current.value) {
+          if (current.left === null) {
+            current.left = newNode;
             return this;
-        } else {
-            let current = this.root
-            while(true) {
-                if (newNode.value === current.value) {
-                    // This tree ignores duplicates, a counter can be added to nodes in the future
-                    return this;
-                }
-                else if (newNode.value < current.value) {
-                    if (current.left === null) {
-                        current.left = newNode;
-                        return this;
-                    } else {
-                        current = current.left
-                    }
-                } else if (newNode.value > current.value) {
-                    if (current.right === null) {
-                        current.right = newNode;
-                        return this;
-                    } else {
-                        current = current.right
-                    }
-                }
-            }
+          } else {
+            current = current.left;
+          }
+        } else if (newNode.value > current.value) {
+          if (current.right === null) {
+            current.right = newNode;
+            return this;
+          } else {
+            current = current.right;
+          }
         }
+      }
+    }
+  }
+
+  search(value) {
+    // checks if a value exists in a tree
+    let current = this.root;
+    while (current != null) {
+      console.log("searching");
+      if (value === current.value) {
+        return current;
+      } else if (value < current.value) {
+        current = current.left;
+      } else if (value > current.value) {
+        current = current.right;
+      }
+    }
+    return -1;
+  }
+  breadthFirstSearch() {
+    // complete each horizontal level before going down a depth
+    let queue = []; // push adds to back, shift removes from front
+    let visited = []; // shows order in which values were searched
+
+    let current = this.root;
+    queue.push(current);
+
+    while (queue.length > 0) {
+      const removedVal = queue.shift(); // gets first element
+      visited.push(removedVal.value);
+
+      // if it has a left value add it to the queue
+      if (removedVal.left) queue.push(removedVal.left);
+
+      // if it has a right value add it to the queue
+      if (removedVal.right) queue.push(removedVal.right);
     }
 
-    search(value) { // checks if a value exists in a tree
-        let current = this.root
-        while(current != null) {
-            console.log("searching")
-            if (value === current.value) {
-                return current
-            }
-            else if (value < current.value) {
-                current = current.left
-            } else if (value > current.value) {
-                current = current.right
-            }
-        }
-        return -1
+    return visited;
+  }
+
+  traverseTreeDfs(node, visited) {
+    if (node === null) return visited
+    else {
+        visited.push(node.value);
+        this.traverseTreeDfs(node.left, visited)
+        this.traverseTreeDfs(node.right, visited)
     }
-    breadthFirstSearch() { 
-        // complete each horizontal level before going down a depth
-        let queue = [] // push adds to back, shift removes from front
-        let visited = [] // shows order in which values were searched
+    return visited;
+  }
 
-        let current = this.root
-        queue.push(current)
+  dfsPreOrder() {
+    // first visit the node, then its entire left side,
+    // then visit its entire right side
 
-        while (queue.length > 0) {
-            const removedVal = queue.shift(); // gets first element
-            visited.push(removedVal.value)
+    let visited = []; // shows order in which values were searched
 
-            // if it has a left value add it to the queue
-            if(removedVal.left) queue.push(removedVal.left)
+    this.traverseTreeDfs(this.root, visited)
 
-            // if it has a right value add it to the queue
-            if(removedVal.right) queue.push(removedVal.right)
-        }
-        
-        return {
-            queue,
-            visited
-        }
-    }
+    return visited;
+  }
 }
 
 let tree = new BinarySearchTree();
 
-const values = [10,6,15,3,8,20]
-values.forEach((item)=>{tree.insert(item)})
+const values = [10, 6, 15, 3, 8, 20];
+values.forEach((item) => {
+  tree.insert(item);
+});
 
-const searchResult = tree.breadthFirstSearch()
-console.log(JSON.stringify(searchResult))
+const searchResult = tree.dfsPreOrder();
+console.log(JSON.stringify(searchResult));
 
 //console.log("Tree structure")
 //console.log(JSON.stringify(tree))
