4f1d979030cb361f39f56bfa83d294e80cd6f08f
Laurell Seville <laurellseville2000@gmail.com>
2023-12-23 15:31:25 +0000

    feat, add insert and find to BST

--- binary-search-trees/BST_intro.js
+++ binary-search-trees/BST_intro.js
@@ -10,14 +10,67 @@ class BinarySearchTree {
     constructor() {
         this.root = null
     }
+
+    insert(value) {
+        var newNode = new Node(value)
+        if (this.root === null) {
+            this.root = newNode;    
+            return this;
+        } else {
+            let current = this.root
+            while(true) {
+                if (newNode.value === current.value) {
+                    // This tree ignores duplicates, a counter can be added to nodes in the future
+                    return this;
+                }
+                else if (newNode.value < current.value) {
+                    if (current.left === null) {
+                        current.left = newNode;
+                        return this;
+                    } else {
+                        current = current.left
+                    }
+                } else if (newNode.value > current.value) {
+                    if (current.right === null) {
+                        current.right = newNode;
+                        return this;
+                    } else {
+                        current = current.right
+                    }
+                }
+            }
+        }
+    }
+
+    search(value) { // checks if a value exists in a tree
+        let current = this.root
+        while(current != null) {
+            console.log("searching")
+            if (value === current.value) {
+                return current
+            }
+            else if (value < current.value) {
+                current = current.left
+            } else if (value > current.value) {
+                current = current.right
+            }
+        }
+        return -1
+    }
 }
 
-let tree = BinarySearchTree();
-tree.root = new Node(10)
-tree.root.right = new Node(29)
-tree.root.right.right = new Node(35)
-tree.root.right.left = new Node(25)
+let tree = new BinarySearchTree();
+tree.insert(10)
+tree.insert(5)
+tree.insert(19)
+tree.insert(21)
+tree.insert(17)
+tree.insert(2)
 
-tree.root.left = new Node(5)
-tree.root.left.right = new Node(8)
-tree.root.left.left = new Node(2)
+console.log("Tree structure")
+console.log(tree)
+
+
+const result = tree.search(17)
+
+console.log("\nsearch result: " + JSON.stringify(result))
